afda644bebe40fddc5dd4bf69a1c4661b2231145
Kat Maldon <katharinamaldon@gmail.com>
2019-05-20 15:14:46 +0000

    Automatically backed up by Learn

--- lib/player.rb
+++ lib/player.rb
@@ -35,3 +35,43 @@ class Player
     puts ""
     board.display
   end
+
+
+private
+
+  def check_priority(board) 
+    flag = true
+
+    x_name = "X"
+    o_name = "O"
+
+    o_position = position_priority(board, o_name) 
+
+    if !o_position.nil?
+      return o_position
+    end
+
+    x_position = position_priority(board, x_name)
+
+    if !x_position.nil?
+      return x_position
+    end
+
+    while flag do
+      random_position = 1 + rand(8)
+      if board.positions_with_values["#{random_position}"] != "X" and board.positions_with_values["#{random_position}"] != "O"
+        board.positions_with_values["#{random_position}"] = "O"
+        return random_position
+        flag false
+      end
+    end
+  end
+
+  def position_priority(board, mark)
+    Board::WINNING_PLACES.each do |winning_place|
+      priority_positions_order = [[0, 1, 2], [0, 2, 1], [1, 2, 0]]
+      priority_positions_order.each do |priority|
+        if (board.positions_with_values["#{winning_place[priority[0]]}"] == mark) and (board.positions_with_values["#{winning_place[priority[1]]}"] == mark)
+          if board.positions_with_values["#{winning_place[priority[2]]}"] == " "
+            return winning_place[priority[2]]
+          end
